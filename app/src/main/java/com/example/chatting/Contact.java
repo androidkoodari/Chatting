ff93ee450e63c568f9c21267b8448d16b7360e53
androidkoodari <artonmailit@gmail.com>
2019-01-24 13:28:39 +0200

    Contact and Contact model added.

---
+++ app/src/main/java/com/example/chatting/Contact.java
@@ -0,0 +1,25 @@
+package com.example.chatting;
+
+
+public class Contact {
+
+    private String jid;
+
+    public Contact(String contactJid)
+    {
+        jid = contactJid;
+    }
+
+
+    public String getJid()
+    {
+        return jid;
+    }
+
+    public void setJid(String jid)
+    {
+        this.jid = jid;
+
+    }
+
+}
